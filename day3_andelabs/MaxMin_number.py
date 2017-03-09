2584bef1872e64d4c1196196b40e90bb574c1a70
Everlyne <Everlyne>
2017-03-09 15:43:07 +0300

    day1 andelalabs

--- day3_andelabs/MaxMin_number.py
+++ day3_andelabs/MaxMin_number.py
@@ -1,4 +1,4 @@
-def find_max_min(alist):
+def find_max_min(alist):#first define function
 	if max(alist)==min(alist):
 		blist=[len(alist)]
 		return blist
---
+++ day3_andelabs/api.py
@@ -0,0 +1,11 @@
+import requests
+
+print ("Your direction is")
+
+origin= input ("Enter your origin")
+
+destination=input ("Enter your destination")
+
+direction=direction.get("https://maps.googleapis.com/maps/api/directions/json?origin=Toronto&destination=Montreal&key=AIzaSyDGaCLRyVSyqbqyMJ6KOtKPjJWQB4wyn4I")
+
+print response.json()
