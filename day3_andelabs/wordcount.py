66475352195576bd833fa15b2041ea23aebe75e4
Everlyne <Everlyne>
2017-03-09 18:30:12 +0300

    day3 andelalabs

--- day3_andelabs/wordcount.py
+++ day3_andelabs/wordcount.py
@@ -1,1 +1,17 @@
+def words(this_is_a_string):
+
+	mydict={}
+	mysplit=this_is_a_string.split()
+
+	if len(mysplit)==1:
+		mydict[mysplit[0]]=1
+		return mydict
+
+	for item in mysplit:
+		if item.isdigit():
+			item=int(item)
+			mydict[item]=mysplit.count(str(item))
+	return mydict
+
+
 
